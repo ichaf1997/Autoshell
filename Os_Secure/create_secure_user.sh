138a55540c592d1e02448e93d929e787a48ac334
Chaff <2279746031@qq.com>
2019-11-08 09:06:54 +0800

    Secure scripts

---
+++ Os_Secure/create_secure_user.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# --------------------------------------------------------------------- #
+
+# This script will forbid root login and create general user for secure
+
+# --------------------------------------------------------------------- # 
+
+     # Create general user
+     while true
+     do
+          read -p "General user : " user
+          id $user >/dev/null 2>&1
+          [ "$?" == "0" ] && break || echo "$user is already exist !"
+     done
+     while true
+     do
+          read -p "$user password : " pass
+          [ -n "$pass" ] && break || echo "password is empty !"          
+     done    
+     useradd $user 
+     echo "$pass"|passwd --stdin ${user}
+     # Add sudo authentication
+     [ "$(cat /etc/sudoers | grep "$user"|wc -l)" == "0" ] && echo "$user  ALL=(ALL)      NOPASSWD: ALL" >> /etc/sudoers
+     clear
+     echo -e "general user\t$user"
+     echo -e "general pass\t$pass"
+     echo "general user create completely !"          
+     # Deny login with root
+     [ "$(cat /etc/ssh/sshd_confi|grep "^PermitRootLogin"|wc -l)" == "1" ] && sed -i 's/^PermitRootLogin/PermitRootLogin no/g' || echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+     systemctl restart sshd
+     echo
+     echo "Root is forbidden from remote link . instead of , use general user to login system is a safe way ."
+     echo "you can get root privileges by command : sudo su -"
---
+++ Os_Secure/fail_login_mon.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# ---------------------------------------------------------- #
+# this script will monitor login . 
+# if login failed over some times , add this ip to iptables
+# ---------------------------------------------------------- #
+
+# Input setting
+max_failed_times=5       
+interval=30              ## Default unit : second
+mon_log=/tmp/failed.log
+
+# Ip in white_list will not be add to iptables 
+white_list_enable=1         ## value 1 means open white_list , value 0 means close white_list
+white_list=(192.168.142.86) ## IP Arrary . notice that IP format is OK , use blank as separator if you want add more than one ip in white_list
+
+[ ! -e "$mon_log" ] && touch $mon_log
+echo 
+echo -e "Time\t$(date +%F\ %T)" >> $mon_log
+echo "Start monitoring ..." >> $mon_log
+
+while true
+do  
+    for line in $(lastb|awk 'BEGIN{OFS=":"}/[.]/{COUNT[$3]++}END{for(ip in COUNT){print COUNT[ip],ip}}')
+    do
+        if [ "$(echo $line|cut -d ":" -f 1)" -ge "$max_failed_times" ];then
+            if [ "$(cat $mon_log|grep "$(echo $line|cut -d ":" -f 2)"|wc -l)" == "0" ];then
+                if [ "$white_list_enable" == "1" ];then
+                   for (i=0;i<${#white_list[*]};i++)
+                   do
+                     [ "${white_list[$i]}" == "$(echo $line|cut -d ":" -f 2)" ] && value=1
+                   done
+                   if [ "$value" != "1" ];then
+                        iptables -A INPUT -s $(echo $line|cut -d ":" -f 2) -j DROP >/dev/null 2>&1
+                        echo "$(date +%F\ %T) - add $(echo $line|cut -d ":" -f 2) to iptables (failed retry = $(echo $line|cut -d ":" -f 1))" >> $mon_log
+                   else
+                        echo "$(date +%F\ %T) - add $(echo $line|cut -d ":" -f 2) is in the white_list (failed retry = $(echo $line|cut -d ":" -f 1)) without any handle." >> $mon_log   
+                   fi   
+                else
+                   iptables -A INPUT -s $(echo $line|cut -d ":" -f 2) -j DROP >/dev/null 2>&1
+                   echo "$(date +%F\ %T) - add $(echo $line|cut -d ":" -f 2) to iptables (failed retry = $(echo $line|cut -d ":" -f 1))" >> $mon_log
+                fi
+            fi
+        fi
+    done
+    sleep $interval
+done
+
